3ba484f6a857103444eeb63600df1c45b2b5cb53
Georgios Lamprou <127976930+geolamprou@users.noreply.github.com>
2025-01-13 13:11:17 +0200

    Update source_code_part_1.R

--- source_code_part_1.R
+++ source_code_part_1.R
@@ -89,24 +89,41 @@ changes$`Change in measure`[changes$`Change in measure` == 1] <- 'A measure or t
 changes$`Change in measure`[changes$`Change in measure` == 2] <- 'Measure is either re-introduced, extended or strengthened/intensified'
 changes$`Change in measure`[changes$`Change in measure` == 3] <- 'A measure is eased or lifted'
 
-changes_distribution <- changes %>%
-  group_by(`Change in measure`) %>%
-  count(`Change in measure`)
+ first_measures_changes_distribution <- changes %>%
+    group_by(`Measure Category`) %>%
+    dplyr::filter(`Change in measure` =='A measure or type of measure is put in place for the first time')%>%
+    count(`Change in measure`)
+  
+  g11 <- ggplot(first_measures_changes_distribution, aes(y= `Measure Category`, x = n, fill = `Measure Category`))+
+    geom_bar(stat='identity', position = 'dodge')+
+    ggtitle("Measures that Intorduced for First Time in Each Category") +
+    theme(
+      plot.title = element_text(hjust = 0.5)
+    )+
+    xlab("Count") +
+    ylab("Measure Category") +
+    geom_text(aes(label = n), position = position_dodge(width = 0.9), hjust = -0.3, colour = "black", size = 6)+
+    theme_hc()+
+    scale_fill_paletteer_d("ggthemes::Classic_Blue_Red_12")
 
-g11 <- ggplot(changes, aes(fill=`Change in measure`, x = `Measure Category`))+
-  geom_bar(stat='count', position = 'dodge')+
-  ggtitle("Type of Change in each Measures' Category") +
-  theme(
-    plot.title = element_text(hjust = 0.5)
-  )+
-  xlab("Measures Categories") +
-  ylab("Count") +
-  geom_text(stat = 'count', aes(label = ..count..), position = position_dodge(width = 0.9), vjust = -0.3, colour = "black") +
-  theme_hc()+
-  scale_fill_paletteer_d("ggthemes::Classic_Blue_Red_12")+
-  theme(axis.text.x = element_text(angle = 18, vjust = 1, hjust = 1, size = 11))
+  g11
 
-g11
+first_measures_changes <- changes %>%
+    group_by(`Measure Category`) %>%
+    dplyr::filter(`Change in measure` !='A measure or type of measure is put in place for the first time')
+  
+g12 <- ggplot(first_measures_changes, aes(y= `Measure Category`, fill = `Change in measure`))+
+    geom_bar(stat='count', position = 'dodge')+
+    ggtitle("Distribution of Measures that Introduced for the First Time based on Type of Changes") +
+    theme(
+      plot.title = element_text(hjust = 0.5)
+    )+
+    xlab("Count") +
+    ylab("Measure Category") +
+    geom_text(stat = 'count', aes(label = ..count..), position = position_dodge(width = 0.9), hjust = -0.3, colour = "black", size = 6)+
+    theme_hc()+
+    scale_fill_paletteer_d("ggthemes::Classic_Blue_Red_12")
+g12
 
 measures_category_subcategory <- data %>%
   dplyr::select(`Measure Category`, `Measure Subcategory`, Measure)%>%
@@ -141,6 +158,8 @@ cities <- local_measures_for_map %>%
 
 ggwordcloud(cities$`Region that measure adapted`, cities$n)
 
+The Map
+
 local_measures_for_map_stats <- local_measures_for_map %>%
   dplyr::select(`Region that measure adapted`, `Level of enforcement`, `Starting Year`, `Ending Year`)
 
@@ -161,7 +180,6 @@ local_measures_for_map_stats_final <- local_measures_for_map_stats_final[!is.na(
 
 local_measures_for_map_stats_final$color <- ifelse(local_measures_for_map_stats_final$`Level of enforcement` == "Measure is a requirement", "darkred",
                                                    ifelse(local_measures_for_map_stats_final$`Level of enforcement` == "Not applicable to the measure", "lightblue", "darkgreen"))
-
 
 leaflet(local_measures_for_map_stats_final) %>%
   addTiles() %>%
